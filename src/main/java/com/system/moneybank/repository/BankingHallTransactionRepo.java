5f802865cec10e830fe30ef1c04b97a57d2b717b
AbeebProPlus <mdahlat94@gmail.com>
2023-06-30 16:45:06 +0100

    implementing officer services

---
+++ src/main/java/com/system/moneybank/repository/BankingHallTransactionRepo.java
@@ -0,0 +1,8 @@
+package com.system.moneybank.repository;
+
+
+import com.system.moneybank.models.BankingHallTransaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BankingHallTransactionRepo extends JpaRepository<BankingHallTransaction, String> {
+}
---
+++ src/main/java/com/system/moneybank/service/OfficerService.java
@@ -0,0 +1,5 @@
+package com.system.moneybank.service;
+
+public interface OfficerService {
+
+}
